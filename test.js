cc56f1cf63d93cc60ad4b634f5d8e6231cde96d9
John Hansen <aterxerxes@gmail.com>
2023-05-21 08:02:21 -0600

    Using fs import for json files in testing.

--- test.js
+++ test.js
@@ -1,7 +1,12 @@
-import mapper_all from './mapper_all.json' assert { type: 'json' }
-import mapper_none from './mapper_none.json' assert { type: 'json' }
-import source from './data1.json' assert { type: 'json' }
+import fs from 'fs/promises'
+
 import { map } from './index.js'
+
+const mapper_all = JSON.parse(await fs.readFile('mapper_all.json'))
+const mapper_none = JSON.parse(await fs.readFile('mapper_none.json'))
+const source = JSON.parse(await fs.readFile('data1.json'))
 
 console.log(map({ mapper: mapper_all, source, }))
 console.log(map({ mapper: mapper_none, source, }))
+
+
